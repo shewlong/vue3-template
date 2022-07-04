e8da209beff3944cf149b9228561b9f7b843b7dc
刘小龙 <934307289@qq.com>
2022-07-04 16:48:33 +0800

    修改build路径，以便github-pages使用

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+  build:{outDir:'docs'} //修改build路径，以便github-pages使用
 })
